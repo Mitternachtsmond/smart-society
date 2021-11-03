b490d68fbfb63daf51fa262809c2335bcf474727
Mukul Jain <79629333+mukulvain@users.noreply.github.com>
2021-11-03 16:52:53 +0530

    Delete Vihaan directory

--- Vihaan/payments/schedule/updater.py
+++
@@ -1,8 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from payments.views import *
-from django.http import request
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(addMaintenance, 'cron', day='1', hour='0', id="monthly_trigger", replace_existing=True)
-    scheduler.start() 
-# to use --noreload with runserver
