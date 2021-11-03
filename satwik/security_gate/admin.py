2ce39cab2d5e0399a2c6e64943ec9546664750e7
Mukul Jain <79629333+mukulvain@users.noreply.github.com>
2021-11-03 17:32:21 +0530

    Delete satwik directory

--- satwik/security_gate/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import parking,gate_log,member
-admin.site.register(parking)
-admin.site.register(gate_log)
-admin.site.register(member)
-
--- satwik/security_gate/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SecurityGateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'security_gate'
