765b570ea0b496d56d1f3b019a3b75f0476eb9e8
Mukul Jain <mukul.jain1906@yahoo.com>
2021-11-13 16:55:37 +0530

    No change announcements

--- api/society_info/models.py
+++ api/society_info/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Property_Info(models.Model):
+    def __init__(self, *args, **kwargs):
+        super(Property_Info, self).__init__(*args, **kwargs)
+        self.__original_property_type = self.property_type
+        self.__original_maintenance = self.maintenance
+        self.__original_covered_area = self.covered_area
+
     property_type = models.CharField(
         _("Property Type"), max_length=25, primary_key=True
     )
@@ -21,8 +27,12 @@ class Property_Info(models.Model):
 
 
 class Inventory(models.Model):
-    item = models.CharField(_("Inventory Item"),
-                            max_length=50, primary_key=True)
+    def __init__(self, *args, **kwargs) -> None:
+        super(Inventory, self).__init__(*args, **kwargs)
+        self.__original_item = self.item
+        self.__original_quantity = self.quantity
+
+    item = models.CharField(_("Inventory Item"), max_length=50, primary_key=True)
     quantity = models.IntegerField(
         _("Quantity"),
     )
@@ -45,8 +55,7 @@ class Announcement(models.Model):
     s_no = models.AutoField(primary_key=True)
     date = models.DateTimeField(_("Date and Time"), auto_now_add=True)
     author = models.CharField(_("Announcement Made by"), max_length=25)
-    category = models.CharField(
-        _("Category"), max_length=25, choices=categories)
+    category = models.CharField(_("Category"), max_length=25, choices=categories)
     description = models.TextField(_("Description"), null=True, blank=True)
 
     def save(self, *args, **kwargs):
--- api/society_info/signals.py
+++ api/society_info/signals.py
@@ -6,21 +6,32 @@ from .models import Announcement, Inventory, Property_Info
 
 @receiver(post_save, sender=Property_Info)
 def save_property_info(sender, instance, created, **kwargs):
-    if created:
-        description = (
-            "Property Information Created:\nProperty Type = %s\nMaintenance = %s\nCovered_Area = %s"
-            % (instance.property_type, instance.maintenance, instance.covered_area)
-        )
-    else:
-        description = (
-            "Property Information Updated:\nProperty Type = %s\nMaintenance = %s\nCovered_Area = %s"
-            % (instance.property_type, instance.maintenance, instance.covered_area)
-        )
-    Announcement.objects.create(
-        author="Admin",
-        category="Notification",
-        description=description,
+    changed_property_type = (
+        instance.property_type != instance._Property_Info__original_property_type
     )
+    changed_maintenance = (
+        instance.maintenance != instance._Property_Info__original_maintenance
+    )
+    changed_covered_area = (
+        instance.covered_area != instance._Property_Info__original_covered_area
+    )
+
+    if changed_property_type or changed_maintenance or changed_covered_area:
+        if created:
+            description = (
+                "Property Information Created:\nProperty Type = %s\nMaintenance = %s\nCovered_Area = %s"
+                % (instance.property_type, instance.maintenance, instance.covered_area)
+            )
+        else:
+            description = (
+                "Property Information Updated:\nProperty Type = %s\nMaintenance = %s\nCovered_Area = %s"
+                % (instance.property_type, instance.maintenance, instance.covered_area)
+            )
+        Announcement.objects.create(
+            author="Admin",
+            category="Notification",
+            description=description,
+        )
 
 
 @receiver(pre_delete, sender=Property_Info)
@@ -39,21 +50,24 @@ def delete_property_info(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Inventory)
 def save_inventory(sender, instance, created, **kwargs):
-    if created:
-        description = "Inventory Item Created:\nItem = %s\nQuantity = %s" % (
-            instance.item,
-            instance.quantity,
+    changed_item = instance.item != instance._Inventory__original_item
+    changed_quantity = instance.quantity != instance._Inventory__original_quantity
+    if changed_item or changed_quantity:
+        if created:
+            description = "Inventory Item Created:\nItem = %s\nQuantity = %s" % (
+                instance.item,
+                instance.quantity,
+            )
+        else:
+            description = "Inventory Item Updated:\nItem = %s\nQuantity = %s" % (
+                instance.item,
+                instance.quantity,
+            )
+        Announcement.objects.create(
+            author="Admin",
+            category="Notification",
+            description=description,
         )
-    else:
-        description = "Inventory Item Updated:\nItem = %s\nQuantity = %s" % (
-            instance.item,
-            instance.quantity,
-        )
-    Announcement.objects.create(
-        author="Admin",
-        category="Notification",
-        description=description,
-    )
 
 
 @receiver(pre_delete, sender=Inventory)
